3033d30d11475316f7746d6ab7793e0039884ce1
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-09 08:23:29 -0300

    Requisitos, testes funcionais e unitários

--- setup/setup/tests.py
+++ setup/setup/tests.py
@@ -1,6 +1,17 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+import selenium
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
-        pass
+        self.browser = webdriver.Chrome('/home/devcead/Documentos/Projetos/Alura/TDD no Django 3/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_abre_janela_do_chrome(self):
+        self.browser.get(self.live_server_url)
+
+    def test_deu_ruim(self):
+        """Teste de Exemplode Erro"""
+        self.fail('Teste Falhou - Deu ruim mesmo')
