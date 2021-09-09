281a32d5d30b4921185e3a7c1bfd5b0365fe94e0
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-09 14:03:00 -0300

    Teste de unidade na view e model

---
+++ setup/animais/tests/test_model.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, RequestFactory
+from animais.models import Animal
+
+class AnimalModelTestCase(TestCase):
+    def setUp(self):
+        self.animal = Animal.objects.create(
+            nome_animal= 'Leão',
+            predador = 'Sim',
+            venenoso = 'Sim',
+            domesticado = 'Não'
+
+        )
+
+    def test_animal_cadastrado_com_caracteristicas(self):
+        """Teste que mostra se o animal está cadastrado com suas respectivas características"""
+        self.assertEqual(self.animal.nome_animal, "Leão")
