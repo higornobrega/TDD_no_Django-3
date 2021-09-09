f32cbee945051dc415350ce456d5ebf92a0cf32d
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-09 09:29:49 -0300

    RequestFactory e templates

--- setup/setup/tests.py
+++ setup/setup/tests.py
@@ -9,9 +9,18 @@ class AnimaisTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def test_abre_janela_do_chrome(self):
-        self.browser.get(self.live_server_url)
+    # def test_abre_janela_do_chrome(self):
+    #     self.browser.get(self.live_server_url)
 
-    def test_deu_ruim(self):
-        """Teste de Exemplode Erro"""
-        self.fail('Teste Falhou - Deu ruim mesmo')
+    # def test_deu_ruim(self):
+    #     """Teste de Exemplode Erro"""
+    #     self.fail('Teste Falhou - Deu ruim mesmo')
+
+    def test_buscando_um_novo_animal(self):
+        """
+        Teste se o usuário encontra um animal pesquisando
+        """
+        home_page = self.browser.get(self.live_server_url + '/')
+
+        brand_element = self.browser.find_element_by_css_selector('.navbar')
+        self.assertEqual('Busca Animal', brand_element.text)
