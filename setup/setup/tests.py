30228919b85007295515b9de87337b3c27098531
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-09 10:52:18 -0300

    Template e cenário de teste

--- setup/setup/tests.py
+++ setup/setup/tests.py
@@ -1,7 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 import selenium
-
+#import time
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/devcead/Documentos/Projetos/Alura/TDD no Django 3/chromedriver')
@@ -24,3 +24,13 @@ class AnimaisTestCase(LiveServerTestCase):
 
         brand_element = self.browser.find_element_by_css_selector('.navbar')
         self.assertEqual('Busca Animal', brand_element.text)
+
+        buscar_animal_input = self.browser.find_element_by_css_selector('input#buscar-animal')
+        self.assertEqual(buscar_animal_input.get_attribute('placeholder'), 'Exemplo: leão')
+        buscar_animal_input.send_keys('leão')
+        #time.sleep(2)
+
+        self.browser.find_element_by_css_selector('form button').click()
+
+        caracteristicas = self.browser.find_elements_by_css_selector('.result-description')
+        self.assertGreater(len(caracteristicas), 3)
